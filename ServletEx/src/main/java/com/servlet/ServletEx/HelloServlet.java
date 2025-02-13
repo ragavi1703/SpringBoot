863df86ffcd14e887bf7cfd17cbf91ce457a48ae
ragavi1703 <108537562+ragavi1703@users.noreply.github.com>
2025-02-13 15:50:47 +0530

    Add files via upload

---
+++ ServletEx/src/main/java/com/servlet/ServletEx/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.servlet.ServletEx;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException {
+		System.out.println("In service");
+		
+		res.setContentType("text/html");
+		
+		PrintWriter out = res.getWriter();
+		out.println("<h2><b>Hello World</b></h2>");
+		
+		//res.getWriter().println("<h2><b>Hello World</b></h2>");
+		
+	}
+
+}
