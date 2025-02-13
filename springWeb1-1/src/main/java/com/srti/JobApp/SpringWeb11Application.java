9cb6d0c47c352cb01ba52eed604f6d8dc2013a5e
ragavi1703 <108537562+ragavi1703@users.noreply.github.com>
2025-02-13 15:55:13 +0530

    Add files via upload

---
+++ springWeb1-1/src/main/java/com/srti/JobApp/SpringWeb11Application.java
@@ -0,0 +1,13 @@
+package com.srti.JobApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeb11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeb11Application.class, args);
+	}
+
+}
---
+++ springWeb1-1/src/test/java/com/srti/JobApp/SpringWeb11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srti.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeb11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
